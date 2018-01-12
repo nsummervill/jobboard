d082f866d668f9c9a3db1591c704be1c2328180b
Nicholas Summervill <nsummervill@yahoo.com>
2018-01-12 09:00:33 -0600

    Installed email functionality

--- JobBoard.UI.MVC/Controllers/HomeController.cs
+++ JobBoard.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using JobBoard.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Diagnostics;
 
 namespace JobBoard.UI.MVC.Controllers
 {
@@ -23,31 +27,46 @@ namespace JobBoard.UI.MVC.Controllers
 
             return View();
         }
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult ContactForm( contact)
-        //{
-        //    //Create the body of the email
-        //    string body = string.Format("Name: {0}<br />Email: {1}", contact.Name, contact.Email);
-        //    //Create and configure a MailMessage object
-        //    MailMessage msg = new MailMessage("no-reply@summdev.com", "nsummervill@yahoo.com", "Email", body);
-        //    //Configure msg
-        //    msg.IsBodyHtml = true;
-        //    msg.Priority = MailPriority.High;
-        //    //Create and configure an SmtpClient object
-        //    SmtpClient client = new SmtpClient("mail.summdev.com");
-        //    client.Credentials = new NetworkCredential("no-reply@summdev.com", "Skater09!");
-        //    //Send the email
-        //    using (client)
-        //    {
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ContactForm(ContactViewModel contact)
+        {
+            //Create the body of the email
+            string body = string.Format("Name: {0}<br />Email: {1}<br />Subject: {2}<br />Message: {3}", contact.Name, contact.Email, contact.Subject, contact.Message);
+            Debug.WriteLine(body);
+            //Create and configure a MailMessage object
+            MailMessage msg = new MailMessage("no-reply@summdev.com", "nsummervill@yahoo.com", "Email", body);
+            //Configure msg
+            msg.IsBodyHtml = true;
+            msg.Priority = MailPriority.High;
+            //Create and configure an SmtpClient object
+            SmtpClient client = new SmtpClient("mail.summdev.com");
+            client.Credentials = new NetworkCredential("no-reply@summdev.com", "Skater09!");
+            //Send the email
+            using (client)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        client.Send(msg);
+                        //Step 7: Create ContactConfirmation View
+                        //(Right clicked Views folder and added a View()
+                        return View("Index", contact);
+                    }
+
+                }
+                catch
+                {
+                    ViewBag.ErrorMessage = "There was an error sending your email." + " Please try again";
+
+                }
+
+                return View();
 
 
-        //        client.Send(msg);
-
-        //        return View();
-
-
-        //    }
+            }
         }
+    }
 }
